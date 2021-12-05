c713a2203b260a75a738dd2bb0827b38c2aa097c
taylorvercillo <taylornvcasey@gmail.com>
2021-12-04 21:56:43 -0700

    all done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    let li = document.createElement('li')
+    li.textContent = e.target['new-task-description'].value
+    document.getElementById('tasks').appendChild(li)
+  })
+})
